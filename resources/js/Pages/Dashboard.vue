c5458732d1d1da2d0e087f75f2a2d20a15ee3f54
Harith Iqbal <hrithiqball@gmail.com>
2024-04-22 16:41:01 +0800

    feat: added google map to dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import Map from '@/Pages/Dashboard/Partials/Map.vue';
 </script>
 
 <template>
@@ -23,6 +24,7 @@ import { Head } from '@inertiajs/vue3';
           <div class="p-6 text-gray-900 dark:text-gray-100">
             You're logged in!
           </div>
+          <Map />
         </div>
       </div>
     </div>
---
+++ resources/js/Pages/Dashboard/Partials/Map.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { GoogleMap, Marker } from 'vue3-google-map';
+
+const center = { lat: 3.06834, lng: 101.506318 };
+const markerList = [{ position: { lat: 3.067247, lng: 101.504279 } }];
+</script>
+
+<template>
+  <GoogleMap
+    api-key="AIzaSyAlOC3BikppdLBZA1yP2akQEmAhQok9F9Y"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker
+      v-for="marker of markerList"
+      :key="marker"
+      :options="{
+        position: { lat: marker.position.lat, lng: marker.position.lng },
+      }"
+    />
+  </GoogleMap>
+</template>
