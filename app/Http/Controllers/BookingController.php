aa6a1ab317aa4abc82287fbfac027fea7dc522b0
Harith Iqbal <harithmu@gmail.com>
2024-05-09 17:55:19 +0800

    chore: added controller for storing data
    todo: create form for storing booking data and update the component
    also: add role for user to approve and admin for edit stuff

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -8,14 +8,38 @@ use Inertia\Inertia;
 use Inertia\Response;
 use App\Models\Booking;
 use App\Models\Vehicle;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class BookingController extends Controller
 {
-    // Booking dashboard
-    public function index(): Response {
+    public function index(): Response
+    {
         return Inertia::render('Booking/BookingDashboard', [
-            // 'booking' => Booking::all(),
+            'booking' => Booking::all(),
             'vehicles' => Vehicle::all()
         ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'start_date' => ['required', 'date'],
+            'end_date' => ['required', 'date'],
+            'vehicle_id' => ['required', 'int'],
+            'booked_by' => ['required', 'int'],
+            'notes' => ['nullable', 'string', 'max:255'],
+        ]);
+
+        Booking::create([
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+            'vehicle_id' => $request->vehicle_id,
+            'booked_by' => $request->booked_by,
+            'notes' => $request->notes ?: null,
+            'status' => 'Pending'
+        ]);
+
+        return Redirect::route('booking.index');
     }
 }
--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'start_date',
+        'end_date',
+        'vehicle_id',
+        'booked_by',
+        'status',
+        'notes'
+    ];
 }
