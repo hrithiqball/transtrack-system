782d5d31816bda7cb51aba2666c237cae3bf79fb
Harith Iqbal <harithmu@gmail.com>
2024-06-23 10:13:24 +0800

    db: added user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,40 @@ class DatabaseSeeder extends Seeder
             'role' => 'admin',
             'password' => bcrypt('password')
         ]);
+
+        User::factory()->create([
+            'name' => 'Harith Iqbal',
+            'email' => 'manager@transtrack.com',
+            'role' => 'manager',
+            'password' => bcrypt('password')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Rosmi Razak',
+            'email' => 'personnel@transtrack.com',
+            'role' => 'personnel',
+            'password' => bcrypt('password')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Haiqal',
+            'email' => 'driver@transtrack.com',
+            'role' => 'driver',
+            'password' => bcrypt('password')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Saiful',
+            'email' => 'personnel2@transtrack.com',
+            'role' => 'personnel',
+            'password' => bcrypt('password')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Zahid',
+            'email' => 'driver2@transtrack.com',
+            'role' => 'driver',
+            'password' => bcrypt('password')
+        ]);
     }
 }
